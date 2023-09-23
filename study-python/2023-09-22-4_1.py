4a7cea2f71a66fca66f21af2692cdd3c409f6835
Changguen Kim <rms1359@gmail.com>
2023-09-23 17:09:36 +0900

    python practice

---
+++ study-python/2023-09-22-4_1.py
@@ -0,0 +1,15 @@
+print(-1)
+print(0)
+print(1) # int
+print(1.1) # float
+print('1+1', 1+1)
+print('2-1', 2-1)
+print('4*1', 4*1)
+print('8/2', 8/2)
+
+import math
+print('math.sqrt(4)',math.sqrt(4))
+print('math.pow(4,2)',math.pow(4,2))
+
+import random
+print('random.random', random.random())
