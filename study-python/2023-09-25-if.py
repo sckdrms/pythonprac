b9de4b6aec11811aceb8508efb1624c06ff7e7df
Changguen Kim <rms1359@gmail.com>
2023-09-25 23:07:43 +0900

    4th day

---
+++ study-python/2023-09-25-if.py
@@ -0,0 +1,19 @@
+# 012
+a = 1
+print(0)
+print(1)
+if a == 1:
+    print(2)
+
+# 01
+print('---')
+b = 1
+print(0)
+print(1)
+if b == 2:
+    print(2)
+
+input_id = input('id: ')
+id = 'rms'
+if input_id == id:
+    print('Welcome')
