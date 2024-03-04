5f0f580d9c0d9799d6b2333cf2901f2fed57c9db
Changguen Kim <rms1359@gmail.com>
2024-03-05 01:02:27 +0900

    1st day study node.js. how to make server and using nodemon

---
+++ study-nodejs/server.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const app = express()
+
+app.use(express.static(__dirname + '/public'))
+
+app.listen(8080, () => {
+    console.log('http://localhost:8080 에서 서버 실행중')
+})
+
+app.get('/', (요청, 응답) => {
+  응답.send('반갑다')
+}) 
+
+app.get('/news', (요청, 응답) => {
+  응답.sendfile(__dirname + '/index.html')
+}) 
+
+app.get('/about', (요청, 응답)=>{
+  응답.sendfile(__dirname+'/about.html')
+})
