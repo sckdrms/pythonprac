a5a1df2fb304ffe293889a9a7c6b99119bf5e97f
Changguen Kim <rms1359@gmail.com>
2024-03-20 22:28:51 +0900

    three.js prac

--- study-nodejs/node_apple/server.js
+++ study-nodejs/node_apple/server.js
@@ -24,6 +24,11 @@ app.get('/', (요청, 응답) => {
   응답.send('반갑다')
 }) 
 
+app.get('/car', (요청, 응답)=>{
+  let time = new Date()
+  응답.sendfile(__dirname+'/index.html')
+})
+
 app.get('/news', (요청, 응답) => {
   db.collection('post').insertOne({title:'어쩌구'})
   // 응답.sendfile(__dirname + '/index.html')
